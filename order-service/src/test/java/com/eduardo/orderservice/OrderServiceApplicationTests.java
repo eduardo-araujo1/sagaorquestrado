3e6dd21deafbcc3b240ace090d7e49916df574da
Eduardo Araujo <eduardo1996araujo@gmail.com>
2024-03-05 14:58:48 -0300

    Configs iniciais

---
+++ order-service/src/test/java/com/eduardo/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardo.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ product-validation-service/src/main/java/com/eduardo/productvalidationservice/ProductValidationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.eduardo.productvalidationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductValidationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductValidationServiceApplication.class, args);
+	}
+
+}
