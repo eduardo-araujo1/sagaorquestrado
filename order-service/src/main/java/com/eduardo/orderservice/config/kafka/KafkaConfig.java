c17031c98a3b2c3a2b29861506b28b37908016a9
Eduardo Araujo <eduardo1996araujo@gmail.com>
2024-03-05 19:50:28 -0300

    Adicionando topicos para o kafka nos microservices

--- order-service/src/main/java/com/eduardo/orderservice/config/kafka/KafkaConfig.java
+++ order-service/src/main/java/com/eduardo/orderservice/config/kafka/KafkaConfig.java
@@ -1,6 +1,7 @@
 package com.eduardo.orderservice.config.kafka;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.*;
 
 import java.util.HashMap;
@@ -18,12 +20,21 @@ import java.util.Map;
 @Configuration
 @RequiredArgsConstructor
 public class KafkaConfig {
+
+    private static final Integer PARTITION_COUNT =1;
+    private static final Integer REPLICA_COUNT =1;
+
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServers;
     @Value("${spring.kafka.consumer.group-id}")
-    private String grouId;
+    private String groupId;
     @Value("${spring.kafka.consumer.auto-offset-reset}")
     private String autoOffSetReset;
+    @Value("${spring.kafka.topic.start-saga}")
+    private String startSagaTopic;
+    @Value("${spring.kafka.topic.notify-ending}")
+    private String notifyEndingTopic;
+
 
     @Bean
     public ConsumerFactory<String, String> consumerFactory(){
@@ -33,7 +44,7 @@ public class KafkaConfig {
     private Map<String, Object> consumerProps(){
         var props = new HashMap<String, Object>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, grouId);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, autoOffSetReset);
@@ -55,6 +66,24 @@ public class KafkaConfig {
     @Bean
     public KafkaTemplate<String, String> kafkaTemplate( ProducerFactory<String,String> producerFactory){
         return new KafkaTemplate<>(producerFactory);
+    }
+
+    private NewTopic buildTopic(String name){
+        return TopicBuilder
+                .name(name)
+                .replicas(REPLICA_COUNT)
+                .partitions(PARTITION_COUNT)
+                .build();
+    }
+
+    @Bean
+    public NewTopic startSagaTopic(){
+        return buildTopic(startSagaTopic);
+    }
+
+    @Bean
+    public NewTopic notifyEndingTopic(){
+        return buildTopic(notifyEndingTopic);
     }
 
 }
